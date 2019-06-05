ce1595221e7d4dd10b82244e3a9038c6ed1a1969
devlawper <contact@devlawper.com>
2019-06-05 15:12:43 +0200

    Création entité Customer, Place et Appointment

--- test-laurent/src/Entity/User.php
+++ test-laurent/src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -42,7 +44,17 @@ class User implements UserInterface
     /**
      * @Assert\EqualTo(propertyPath="password", message="Vous n'avez pas tapé le même mot de passe")
      */
-    public $confirm_password; // on ajoute confirm_password sans ORM car il n'a pas de lien avec la BDD
+    public $confirm_password;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Appointment", mappedBy="idUser")
+     */
+    private $appointments;
+
+    public function __construct()
+    {
+        $this->appointments = new ArrayCollection();
+    } // on ajoute confirm_password sans ORM car il n'a pas de lien avec la BDD
 
     public function getId(): ?int
     {
@@ -94,5 +106,36 @@ class User implements UserInterface
     }
     public function getRoles() {
         return ['ROLE_USER'];
+    }
+
+    /**
+     * @return Collection|Appointment[]
+     */
+    public function getAppointments(): Collection
+    {
+        return $this->appointments;
+    }
+
+    public function addAppointment(Appointment $appointment): self
+    {
+        if (!$this->appointments->contains($appointment)) {
+            $this->appointments[] = $appointment;
+            $appointment->setIdUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAppointment(Appointment $appointment): self
+    {
+        if ($this->appointments->contains($appointment)) {
+            $this->appointments->removeElement($appointment);
+            // set the owning side to null (unless already changed)
+            if ($appointment->getIdUser() === $this) {
+                $appointment->setIdUser(null);
+            }
+        }
+
+        return $this;
     }
 }
